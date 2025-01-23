5898d79e9a65beacaa4d27bcc154ada311dcabd2
VishalPukale-GeniaTeq <vishal.pukale@geniateq.com>
2025-01-23 11:46:28 +0530

    image upload backend

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,9 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/products", productsRoutes);
 app.use("/api/upload", uploadRoutes);
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
+
 app.listen(port, ()=>{
     `Server running on port ${port}`
 })
