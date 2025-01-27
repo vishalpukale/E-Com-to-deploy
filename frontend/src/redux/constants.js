51af883986d6cd2a4e19ff95ba1a94ff9cd38078
VishalPukale-GeniaTeq <vishal.pukale@geniateq.com>
2025-01-27 11:15:16 +0530

    Product api created frontend

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,3 +1,6 @@
 export const BASE_URL = '' // proxy
 export const USERS_URL = '/api/users' // proxy
 export const CATEGORY_URL = '/api/category' // proxy
+
+export const PRODUCT_URL = '/api/products'
+export const UPLOAD_URL = '/api/upload'
