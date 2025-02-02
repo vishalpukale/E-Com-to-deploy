169775b965c28c396782c8b1b3f43d315f783c9c
VishalPukale-GeniaTeq <vishal.pukale@geniateq.com>
2025-02-02 10:19:19 +0530

    package

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,10 @@ app.get('/api/config/paypal', (req, res)=> {
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 app.listen(port, ()=>{
     `Server running on port ${port}`
